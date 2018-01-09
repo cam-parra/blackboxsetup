d974d48923cfec8f1d61aa5c5c25bca26cee93f7
Cam Parra <caeparra@gmail.com>
2018-01-09 14:17:04 -0700

    just stuff

--- 34500.cpp
+++ 34500.cpp
@@ -1,29 +1,34 @@
 /***********************************************************************
- * Program:
- *    Lab 00, Practice Lab Submission
- *    Brother Jones, CS345
- * Author:
- *    your name
- * Summary: 
- *    Enter a brief description of your program here!  Please note that if
- *    you do not take the time to fill out this block, YOU WILL LOSE POINTS.
- ************************************************************************/
-
+* Program:
+*    Lab 00, Practice Lab Submission
+*    Brother Jones, CS345
+* Author:
+*    Cam Parra
+* Summary:
+*    Just a simple program that checks for times a integer is entered.
+*    If the user puts in a 0 the process stops. It uses user defines
+*    to make the function smaller (sorry to those reading the code).
+*    It also exploits the for loop to do all computations and logic
+*    with the AND operator.
+************************************************************************/
 #include <iostream>
 using namespace std;
-/**********************************************************************
- * Add text here to describe what the function "main" does. Also don't forget
- * to fill this out with meaningful text or YOU WILL LOSE POINTS.
- ***********************************************************************/
-int main()
 
+#define PROMPT "Enter an integer: "
+#define ZERO "No non-zero integers were entered.\n"
+#define ENTERED "You entered "
+#define NUM " integer"
+
+/**********************************************************************
+* Logic for AND is that only  will pass if all conditions are met. So
+* this code works because COUT and CIN will test true but will fail
+* when i = 0. Then this code then uses conditional ternarys to figure
+* out how to display the result.
+***********************************************************************/
+int main()
 {
-  int input;
-  int count = 0;
-  while(cout << "Enter an integer: " &&cin >> input && input !=0)
-    {
-      count++;
-      (count == 0) ? cout <<"" : cout << count <<endl;
-    }
-  return 0;
+   int c = 0;
+   for (int i = 0; cout << PROMPT  && cin >> i && i != 0;++c);
+   (c == 0) ? cout << ZERO : (c == 1) ? cout << ENTERED << c << NUM << ".\n" :
+      cout << ENTERED << c << NUM << "s.\n";
 }
